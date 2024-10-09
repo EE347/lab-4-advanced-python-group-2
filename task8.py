0c36714ad49f6e858981171e2b52fd57cc6e768e
Jack Connolly <j.connolly36@universityofgalway.ie>
2024-10-09 16:17:55 +0100

    Phil is cool

--- task8.py
+++ task8.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-sin_wave = np.load("task7_sin.py.npy")
-cos_wave = np.load("task7_cos.py.npy")
+sin_wave = np.load("task7_sin.npy")
+cos_wave = np.load("task7_cos.npy")
 
 x = np.linspace(0, 10, 101)
 plt.plot (x, sin_wave, label = "Sine", color = 'b')
-plt.plot (x, cos_wave, label = "Cosine")
+plt.plot (x, cos_wave, label = "Cosine", color = 'r')
 plt.legend()
 plt.show()
