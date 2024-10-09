8ffccda3610dcfa956791e42477d85d37611c281
Jack Connolly <j.connolly36@universityofgalway.ie>
2024-10-09 15:50:53 +0100

    Jack is cool cool cool cool cool cool

--- task5.py
+++ task5.py
@@ -1,2 +1,14 @@
 import csv
 
+with open("task5.csv", "w", newline = "") as file:
+    writer = csv.writer(file)
+    while True:
+        name = input("Enter your name Beech(Type quit to stop):")
+        if name.lower() == 'quit':
+            break
+        writer.writerow([name])
+
+with open ("task5.csv", "r") as file:
+    reader = csv.reader(file)
+    for row in reader:
+        print(row[0])
