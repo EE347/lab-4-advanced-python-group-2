9aba4f9d45c7fd217484cc3600c6797a05003abc
Jack Connolly <j.connolly36@universityofgalway.ie>
2024-10-09 16:01:51 +0100

    Jack is cool cool cool cool cool cool cool cool

--- task7.py
+++ task7.py
@@ -5,5 +5,5 @@ x = np.linspace(0, 10, 101)
 sin_wave = np.sin(x)
 cos_wave = np.cos(x)
 
-np.save("task7_sin.py", sin_wave)
-np.save("task7_cos.py", cos_wave)
+np.save("task7_sin.npy", sin_wave)
+np.save("task7_cos.npy", cos_wave)
